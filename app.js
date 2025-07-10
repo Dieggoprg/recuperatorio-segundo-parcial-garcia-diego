7c7d7aed3ebdeff64b0c5115d7a32df63cbbf851
Garcia Diego <garciadiegogabriel.gn@gmail.com>
2025-07-10 15:34:22 -0300

    SE AGREGÓ LA CONEXIÓN A LA BASE DE DATOS

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from "express";
 import "dotenv/config";
+import { initDB } from "./src/config/database.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+initDB().then( ()=>{
+    app.listen(PORT, ()=> {
+        console.log("Server corriendo en el puerto: ",PORT)
+    })
+})
+
